5f6fc5f01c78ac1ccade1988f749133d49af3d7c
Abhishek1744 <abhishekmujumale1744@gmail.com>
2025-01-24 23:34:26 +0530

    day3of31

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from routes.user_routes import user_bp
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return {"message": "Welcome to Career Craft API!"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
+
+app.register_blueprint(user_bp, url_prefix="/api")
